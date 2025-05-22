9af019b3b76a5440669632a3656bba93e9bbffe2
Julio Gaekel <jriveragaekel@gmail.com>
2025-05-22 11:47:59 +0000

    Complete exercise object creation

--- Start/Ch 1/definition_start.py
+++ Start/Ch 1/definition_start.py
@@ -3,9 +3,14 @@
 
 
 # TODO: create a basic class
-
+class Book:
+  def __init__(self, title):
+    self.title = title
 
 # TODO: create instances of the class
-
+book1 = Book("Lord of the Rings")
+book2 = Book("Harry Potter")
 
 # TODO: print the class and property
+print(book1)
+print(book1.title)
